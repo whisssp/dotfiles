fcbfdbc98f095b995fad8d77662f6cf1f6512f7f
Whisper <nghia14802@gmail.com>
2026-09-14 15:45:09 +0700

    update call timeout

--- home.nix
+++ home.nix
@@ -51,7 +51,7 @@ in
   programs.starship = {
     enable = true;
     settings = {
-      command_timeout = 300;
+      command_timeout = 1000;
       add_newline = false;
       format = "$directory$git_branch$git_status$cmd_duration$line_break$character";
       character = {
